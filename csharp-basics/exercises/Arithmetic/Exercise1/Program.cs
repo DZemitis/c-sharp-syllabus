4bc8c6c32dc7c5c0f043418cf18e63fc777a6819
DZemitis <daveziemels@gmail.com>
2021-11-23 21:49:16 +0200

    exercise 1 completed

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(CheckIfTrue(5, 20));
+        }
+
+        public static bool CheckIfTrue(int n1, int n2)
+        {
+
+            if (n1 + n2 == 15 || n2 - n1 == 15 || n1 == 15 || n2 == 15 || n1 - n2 == 15)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
