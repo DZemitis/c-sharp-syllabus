7f5c9258ebf7c32524ee35ac0747d0d9fdb658a0
DZemitis <daveziemels@gmail.com>
2021-11-23 20:23:40 +0200

    init

---
+++ csharp-basics/exercises/Arithmetic/exercise 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum *= i;
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
