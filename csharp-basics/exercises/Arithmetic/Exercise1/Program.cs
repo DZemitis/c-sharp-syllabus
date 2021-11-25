89bd2d8b85554ee0e813dc02bf81baacc474fb79
DZemitis <daveziemels@gmail.com>
2021-11-25 21:08:41 +0200

    changes for exercise 1

--- csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -15,12 +15,8 @@ namespace ConsoleApp8
 
         public static bool CheckIfTrue(int number1, int number2)
         {
-            if (number1 + number2 == 15 || number2 - number1 == 15 || number1 == 15 || number2 == 15 || number1 - number2 == 15)
-            {
-                return true;
-            }
-
-            return false;
+            return (number1 + number2 == 15 || number2 - number1 == 15 || number1 == 15 || number2 == 15 ||
+                    number1 - number2 == 15);
         }
     }
 }
