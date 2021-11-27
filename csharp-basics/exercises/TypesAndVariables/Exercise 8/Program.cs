270e0a2161f579e38310370b32fe26217242975e
DZemitis <daveziemels@gmail.com>
2021-11-27 15:54:55 +0200

    Exercise 8 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter minutes to calculate!");
+                int minutes = int.Parse(Console.ReadLine());
+                if (minutes < 0)
+                {
+                    Console.WriteLine("Invalid Value");
+                }
+                else
+                {
+                    int hours = minutes / 60;
+                    int day = hours / 24;
+                    int years = day / 365;
+                    int remainingDays = day % 365;
+                    Console.WriteLine(minutes + " minutes is " + years + " years and " + remainingDays + " days!");
+                }
+        }
+    }
+}
