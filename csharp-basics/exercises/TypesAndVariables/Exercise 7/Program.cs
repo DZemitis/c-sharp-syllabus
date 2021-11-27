c2f2352abc5b356dda04610e521a62710c13087f
DZemitis <daveziemels@gmail.com>
2021-11-27 15:27:05 +0200

    Exercise 7 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            int countUpper = 0;
+            for (int i = 0; i < userInput.Length -1; i++)
+            {
+                if (char.IsUpper(userInput[i])) countUpper++;
+            }
+        }
+    }
+}
