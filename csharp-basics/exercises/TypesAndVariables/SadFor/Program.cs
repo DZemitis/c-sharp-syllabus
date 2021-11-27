4072fba364ad66b858ed1c1a2fa4033c9568452c
DZemitis <daveziemels@gmail.com>
2021-11-27 17:25:19 +0200

    SadFor done

--- csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
@@ -4,23 +4,20 @@ namespace SadFor
 {
     class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Test1();
         }
 
-        private static void Test1()
+        public static void Test1()
         {
-            //fixme so I would compile
-            /*
-            for (i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 if (Math.Sqrt(i) > 2.5)
                 {
                     Console.WriteLine(i);
                 }
             }
-            */
         }
     }
 }
