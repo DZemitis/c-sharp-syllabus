205f086d316619c991c8c8bfb1c1cb60c0d1b22e
DZemitis <daveziemels@gmail.com>
2021-11-28 12:47:19 +0200

    done some steps with TenBillion

--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.CodeDom;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace TenBillion
 {
@@ -8,64 +12,62 @@ namespace TenBillion
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
-
+            
             var input = Console.ReadLine();
+            var numberToValidate = long.Parse(input);
+            
 
-            /*
-            todo - check if Long
-            if (?) 
+            // if (Regex.IsMatch(input, @"^-?([0]{1}\[0-9]+|[1-9]{1}[0-9]*[0-9]+|[0-9]+|0)$"))
+            //if (Regex.IsMatch(input, @"^-?([0-9]+|[0-9])$"))
+            if (numberToValidate < long.MaxValue || numberToValidate > long.MinValue)
             {
                 //todo - check if n is less than zero
-                if (?) 
+                var n = long.Parse(input);
+                if (n < 0)
                 {
                     n *= -1;
                 }
 
-                fixme
-                if (?) 
+                if (n >= 10000000000) 
                 {
                     Console.WriteLine("Number is greater or equals 10,000,000,000!");
                 } 
                 else 
                 {
                     int digits = 1;
-                    if (?) 
+                    switch (input.Length)
                     {
-                        digits = 2;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 3;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 4;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 5;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 6;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 7;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 8;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 9;
-                    } 
-                    else if (?) 
-                    {
-                        digits = 10;
+                        case 1:
+                            digits = 1;
+                            break;
+                        case 2:
+                            digits = 2;
+                            break;
+                        case 3:
+                            digits = 3;
+                            break;
+                        case 4:
+                            digits = 4;
+                            break;
+                        case 5:
+                            digits = 5;
+                            break;
+                        case 6:
+                            digits = 6;
+                            break;
+                        case 7:
+                            digits = 7;
+                            break;
+                        case 8:
+                            digits = 8;
+                            break;
+                        case 9:
+                            digits = 9;
+                            break;
+                        case 10:
+                            digits = 10;
+                            break;
                     }
-
                     Console.WriteLine("Number of digits in the number: " + digits);
                 }
             } 
@@ -73,7 +75,6 @@ namespace TenBillion
             {
                 Console.WriteLine("The number is not a long");
             }
-            */
         }
     }
 }
