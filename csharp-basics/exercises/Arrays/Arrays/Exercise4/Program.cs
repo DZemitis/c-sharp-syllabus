0ca8439e56769480cf8b5719351fddeb636e47b4
DZemitis <daveziemels@gmail.com>
2021-11-29 15:48:20 +0200

    fix for Exercise4

--- csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
@@ -16,7 +16,7 @@ namespace Exercise4
             {
                 1789, 2035, 1899, 1456, 2013,
                 1458, 2458, 1254, 1472, 2365,
-                1456, 2265, 1457, 2456,
+                1456, 2265, 1457, 2456
             };
 
             foreach (int n in myArray)
@@ -24,10 +24,6 @@ namespace Exercise4
                 if (n == 1245)
                 {
                     Console.WriteLine("Contains!");
-                }
-                else
-                {
-                    Console.WriteLine("Does not contain!");
                 }
             }
         }
