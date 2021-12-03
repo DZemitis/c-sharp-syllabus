494367bd17e2833e9a821fa70a374e58252176c6
DZemitis <daveziemels@gmail.com>
2021-12-03 21:10:11 +0200

    Exercise 6 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input > 100)
+            {
+                Console.WriteLine("Max Value? 100");
+                for (int i = 1; i <= 100; i++)
+                {
+                    if (i % 5 == 0 && i % 3 == 0)
+                    {
+                        Console.Write("FizzBuzz ");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.Write("Buzz ");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.Write("Fizz ");
+                    }
+                    else
+                    {
+                        Console.Write(i + " ");
+                    }
+
+                    if (i % 20 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= input; i++)
+                {
+                    if (i % 5 == 0 && i % 3 == 0)
+                    {
+                        Console.Write("FizzBuzz ");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.Write("Buzz ");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.Write("Fizz ");
+                    }
+                    else
+                    {
+                        Console.Write(i + " ");
+                    }
+
+                    if (i % 20 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
