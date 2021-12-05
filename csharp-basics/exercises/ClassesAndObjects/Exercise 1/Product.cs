5cbaa183be2ac55be75b3c95ca6ba03249b5745a
DZemitis <daveziemels@gmail.com>
2021-12-05 19:30:08 +0200

    Exercise_1 done

--- csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
@@ -1,7 +1,35 @@
-﻿namespace Exercise_1
+﻿using System;
+using System.Dynamic;
+using static System.Console;
+
+namespace Exercise_1
 {
     class Product
     {
+        private static string _name;
+        private static double _priceAtStart;
+        private static int _amountAtStart;
 
+        public Product(string name, double price, int amount)
+        {
+            _name = name;
+            _priceAtStart = price;
+            _amountAtStart = amount;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name} price {_priceAtStart:0.00} EUR, amount {_amountAtStart}");
+        }
+
+        public double SetPrice(double newPrice)
+        {
+            return _priceAtStart = newPrice;
+        }
+
+        public int ChangeAmount(int newAmount)
+        {
+            return _amountAtStart = newAmount;
+        }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
@@ -6,7 +6,22 @@ namespace Exercise_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var product1 = new Product("Banana", 1.1, 13);
+            product1.PrintProduct();
+            product1.SetPrice(2);
+            product1.PrintProduct();
+            var product2 = new Product("Logitech mouse", 70.00, 14);
+            product2.PrintProduct();
+            product2.ChangeAmount(50);
+            product2.PrintProduct();
+            var product3 = new Product("iPhone 5s", 999.99, 3);
+            product3.PrintProduct();
+            product3.SetPrice(50);
+            product3.PrintProduct();
+            var product4 = new Product("Epson EB-U05", 440.46, 1);
+            product4.PrintProduct();
+            product4.ChangeAmount(5);
+            product4.PrintProduct();
         }
     }
 }
