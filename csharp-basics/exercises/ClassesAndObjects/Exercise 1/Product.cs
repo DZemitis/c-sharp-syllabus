dae29a92aff3eabbf99e32de956903a2559c43bd
DZemitis <daveziemels@gmail.com>
2021-12-06 17:12:08 +0200

    fix for Exercise_1

--- csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
@@ -1,35 +1,23 @@
-﻿using System;
-using System.Dynamic;
-using static System.Console;
-
-namespace Exercise_1
+﻿namespace Exercise_1
 {
+
     class Product
     {
-        private static string _name;
-        private static double _priceAtStart;
-        private static int _amountAtStart;
-
         public Product(string name, double price, int amount)
         {
             _name = name;
-            _priceAtStart = price;
-            _amountAtStart = amount;
+            PriceAtStart = price;
+            AmountAtStart = amount;
         }
+        private string _name { get; set; }
+        public double PriceAtStart { get; set; }
+        public int AmountAtStart { get; set; }
 
-        public void PrintProduct()
-        {
-            Console.WriteLine($"{_name} price {_priceAtStart:0.00} EUR, amount {_amountAtStart}");
-        }
 
-        public double SetPrice(double newPrice)
-        {
-            return _priceAtStart = newPrice;
-        }
 
-        public int ChangeAmount(int newAmount)
+        public string PrintProduct()
         {
-            return _amountAtStart = newAmount;
+            return $"{_name} price {PriceAtStart:0.00} EUR, amount {AmountAtStart}";
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
@@ -7,32 +7,22 @@ namespace Exercise_1
         static void Main(string[] args)
         {
             var product1 = new Product("Banana", 1.1, 13);
-            product1.PrintProduct();
-            product1.SetPrice(2);
-            product1.PrintProduct();
             var product2 = new Product("Logitech mouse", 70.00, 14);
-            product2.PrintProduct();
-            product2.ChangeAmount(50);
-            product2.PrintProduct();
             var product3 = new Product("iPhone 5s", 999.99, 3);
-            product3.PrintProduct();
-            product3.SetPrice(50);
-            product3.PrintProduct();
             var product4 = new Product("Epson EB-U05", 440.46, 1);
-            product4.PrintProduct();
-            product4.ChangeAmount(5);
-            product4.PrintProduct();
-            Console.WriteLine();
+            product1.AmountAtStart = 5;
+            product2.PriceAtStart = 600;
+
             var products = new Product[]
             {
-                new Product("Banana", 1.1, 13),
-                new Product("Logitech mouse", 70.00, 14),
-                new Product("iPhone 5s", 999.99, 3),
-                new Product("Epson EB-U05", 440.46, 1)
+                product1,
+                product2,
+                product3,
+                product4
             };
             foreach (var prod in products)
             {
-                prod.PrintProduct();
+                Console.WriteLine(prod.PrintProduct());
             }
         }
     }
