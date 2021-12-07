a14abfa5896ca1f435d3f3e768dbd440ae7af59b
DZemitis <daveziemels@gmail.com>
2021-12-07 21:04:08 +0200

    ex 7

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/Dog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    public class Dog
+    {
+        public string Name;
+        public string Sex;
+        public string Father;
+        public string Mother;
+
+        public Dog(string name, string sex)
+        {
+            Name = name;
+            Sex = sex;
+        }
+
+        public Dog(string name, string sex, string father, string mother):this(name,sex)
+        {
+            Father = father ?? "unknown";
+            Mother = mother ?? "unknown";
+        }
+
+        public string fathersName()
+        {
+            return $"{Name}'s dad is {Father ?? "unknown"}";
+        }
+        public string SameMother(Dog name)
+        {
+            return $"{Name} has same mother as {name}";
+        }
+        public bool HasSameMother(Dog dog, Dog otherDog)
+        {
+            if (dog.Mother == otherDog.Mother)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/DogTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_7
+{
+    public class DogTests
+    {
+        public static void Main()
+        {
+            var Max = new Dog("Max", "male");
+            var Rocky = new Dog("Rocky", "male");
+            var Sparky = new Dog("Sparky", "male");
+            var Buster = new Dog("Buster", "male");
+            var Sam = new Dog("Sam", "male");
+            var Lady = new Dog("Lady", "female");
+            var Molly = new Dog("Molly", "female");
+            var Coco = new Dog("Coco", "female");
+            Max.Mother = "Lady";
+            Max.Father = "Rocky";
+            Coco.Mother = "Molly";
+            Coco.Father = "Buster";
+            Rocky.Mother = "Molly";
+            Rocky.Father = "Sam";
+            Buster.Mother = "Lady";
+            Buster.Father = "Sparky";
+            var DogTest = new List<Dog>
+            {
+                Max,
+                Rocky,
+                Sparky,
+                Buster,
+                Sam,
+                Lady,
+                Molly,
+                Coco
+            };
+
+            foreach (var dog in DogTest)
+            {
+                Console.WriteLine($"{dog.fathersName()}");
+                Console.WriteLine($"{dog.SameMother(Rocky)}" + " " + dog.HasSameMother(dog, Rocky));
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
