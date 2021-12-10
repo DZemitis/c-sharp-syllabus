1727c5a59fe82382ef99d7d83a84807ad1aaae57
DZemitis <daveziemels@gmail.com>
2021-12-10 22:37:23 +0200

    Exercise 3 done

---
+++ csharp-basics/exercises/Collections/Exercise 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        public static bool inputHappens = true;
+        public static List<string> names = new List<string>();
+
+        static void Main(string[] args)
+        {
+            inputNames();
+            printNamees();
+        }
+
+        public static void inputNames()
+        {
+            Console.WriteLine("Enter list of names, pres Enter after done");
+
+            while (inputHappens == true)
+            {
+                var input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
+                {
+                    names.Add(input);
+                }
+                else
+                {
+                    inputHappens = false;
+                }
+            }
+        }
+
+        public static void printNamees()
+        {
+            List<string> distinct = names.Distinct().ToList();
+            foreach (var x in distinct)
+            {
+                Console.WriteLine(x);
+
+            }
+        }
+    }
+}
