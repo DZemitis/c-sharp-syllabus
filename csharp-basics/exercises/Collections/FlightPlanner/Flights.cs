91916514e185153975fd9022e18dc0175a600cd8
DZemitis <daveziemels@gmail.com>
2021-12-12 02:08:51 +0200

    init FlightPlanner

---
+++ csharp-basics/exercises/Collections/FlightPlanner/Flights.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlightPlanner
+{
+   public class Flights
+    {
+        public Flights(string beginning, string destination)
+        {
+            Beginning = beginning;
+            Destination = destination;
+        }
+
+
+        public string Beginning { get; set; }
+        public string Destination { get; set; }
+
+        public string ListOfCities()
+        {
+            return $"{Beginning}";
+        }
+        public string DisplayFlights()
+        {
+            return $"From {Beginning} to {Destination}";
+        }
+        public override string ToString()
+        {
+            return $"{Beginning}";
+        }
+    }
+}
--- csharp-basics/exercises/Collections/FlightPlanner/Program.cs
+++ csharp-basics/exercises/Collections/FlightPlanner/Program.cs
@@ -2,21 +2,58 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace FlightPlanner
 {
     class Program
     {
-        private const string Path = "../FlightPlanner/flights.txt";
+        public static List<Flights> flights = new List<Flights>();
+        private const string Path = "../../flights.txt";
+        private static List<string> readText = File.ReadAllLines(Path).ToList();
 
         private static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            AddData();
+            Console.WriteLine("Hello, Welcome to the Flight Planner!" +
+                              "\nTo bring up the list of available Cities to depart from, please pres 1!" +
+                              "\nTo Exit the program, press #");
+            var userInput = Console.ReadKey().KeyChar.ToString();
+            if (userInput == "1")
             {
-                Console.WriteLine(s);
+                GetCityList();
+            }
+            else 
+            {
+                Console.Clear();
+                Console.WriteLine("Come again");
+            }
+            
+        }
+
+        private static void AddData()
+        {
+            foreach (var x in readText)
+            {
+                string[] entries = Regex.Split(x, @" -> ");
+                Flights newRoute = new Flights(entries[0], entries[1]);
+                flights.Add(newRoute);
+            }
+        }
+
+        private static void GetCityList()
+        {
+            var cities = new List<string>();
+            foreach (var x in flights)
+            {
+                cities.Add(x.ListOfCities());
+            }
+
+            List<string> distinct = cities.Distinct().ToList();
+            List<string> distinctList = distinct.Distinct().ToList();
+            foreach (var x in distinctList)
+            {
+                Console.WriteLine(x);
             }
         }
     }
