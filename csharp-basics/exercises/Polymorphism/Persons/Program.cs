2ef28858bb05e573b5d13b9317684d53bac3f732
DZemitis <daveziemels@gmail.com>
2021-12-14 21:50:58 +0200

    Person done

--- csharp-basics/exercises/Polymorphism/Persons/Program.cs
+++ csharp-basics/exercises/Polymorphism/Persons/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Persons
+﻿namespace Persons
 {
     class Program
     {
@@ -10,6 +8,10 @@ namespace Persons
              * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
              * - Display the content of each class using Display() method.
              */
+            Student student1 = new Student(94, "Janis", "Krumins", "Avotu Iela 46", 13);
+            Employee employee1 = new Employee("janitor", "John", "Watson", "Boulevard 2", 14);
+            student1.Display();
+            employee1.Display();
         }
     }
 }
---
+++ csharp-basics/exercises/Polymorphism/Persons/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Persons
+{
+    public class Student : Person
+    {
+        protected double gpa { get; set; }
+
+        public Student(double gpa, string firstName, string lastName, string address, int id)
+        {
+            this.gpa = gpa;
+            base.lastName = lastName;
+            base.address = address;
+            base.firstName = firstName;
+            base.id = id;
+        }
+
+        public new void Display()
+        {
+            Console.WriteLine($"ID{id} : {firstName} {lastName}, lives on {address} has {gpa}");
+        }
+    }
+}
