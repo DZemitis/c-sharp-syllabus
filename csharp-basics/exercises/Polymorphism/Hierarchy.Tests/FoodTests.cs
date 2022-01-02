341d773b858be00fc6224f14908b960342acc5cc
DZemitis <daveziemels@gmail.com>
2022-01-02 20:59:10 +0200

    Hierarchy Tests done

---
+++ csharp-basics/exercises/Polymorphism/Hierarchy.Tests/FoodTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Hierarchy.Tests
+{
+    public class FoodTests
+    {
+        private List<Food> _foodTarget;
+
+        public FoodTests()
+        {
+            _foodTarget = new List<Food>() {new Meat(10), new Vegetable(10)};
+        }
+
+        [Fact]
+        public void ToString_Meat10_ShouldContain()
+        {
+            //Arrange
+            var foodToString = $"Meat 10";
+            
+            //Act
+            var foodGivenNameQTY = _foodTarget[0].ToString();
+
+            //Assert
+            Assert.Contains(foodToString, foodGivenNameQTY);
+        }
+    }
+}
